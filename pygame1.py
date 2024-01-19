3240f34f02f602e82a65f34223b9ebef66451ee9
Roberto <bobbylunaexcl@gmail.com>
2024-01-19 14:56:17 -0500

    Second commit

--- pygame1.py
+++ pygame1.py
@@ -2,8 +2,8 @@ import pygame
 from sys import exit #closes everything once we call it
 
 def display_score():
-    current_time = pygame.time.get_ticks() - start_time #gets the time since the pygame started
-    score_surf = test_font.render(str(current_time), False, (64,64,64))
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time #gets the time since the pygame started
+    score_surf = test_font.render(F"Score: {current_time}", False, (64,64,64))
     score_rect = score_surf.get_rect(center = (400,50))
     screen.blit(score_surf, score_rect)
 
@@ -14,7 +14,7 @@ pygame.display.set_caption("Bobby The Snail Runner") #title for window
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("C:/Users/Rober/Downloads/UltimatePygameIntro-main/UltimatePygameIntro-main/font/Pixeltype.ttf", 50) #font type and font size
 
-game_active = True
+game_active = False
 start_time = 0
 
 sky_surface = pygame.image.load('C:/Users/Rober/Downloads/UltimatePygameIntro-main/UltimatePygameIntro-main/graphics/Sky.png').convert()
@@ -30,6 +30,16 @@ player_rect = player_surf.get_rect(midbottom = (80,300)) #player movement, creat
 # test_surface = pygame.Surface((100, 200)) #origin is at top left
 # test_surface.fill('Red')
 player_gravity = 0
+#intro screen
+player_stand = pygame.image.load("C:/Users/Rober/Downloads/UltimatePygameIntro-main/UltimatePygameIntro-main/graphics/player/player_stand.png").convert_alpha()
+player_stand = pygame.transform.scale2x(player_stand) #only one argument with 2x
+player_stand_rect = player_stand.get_rect(center = (400,200))
+
+game_name = test_font.render("Bobby The Snail Runner", False, (111,196,169))
+game_name_rect = game_name.get_rect(center = (400,80))
+
+game_message = test_font.render("Press space to run", False, (111,196,169))
+game_message_rect = game_message.get_rect(center=(400,320))
 
 while True:
     for event in pygame.event.get(): #this method gets all the events so as the for loop
@@ -49,7 +59,7 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800 #resets position of snail
-                start_time = pygame.time.get_ticks()
+                start_time = int(pygame.time.get_ticks() / 1000)
 
     if game_active:
         screen.blit(sky_surface,(0,0)) #position on the display surface
@@ -74,7 +84,10 @@ while True:
         if snail_rect.colliderect(player_rect):
             game_active = False
     else:
-        screen.fill("red")
+        screen.fill((94,129,162))
+        screen.blit(player_stand, player_stand_rect)
+        screen.blit(game_name,game_name_rect)
+        screen.blit(game_message, game_message_rect)
         #keyboard input
         # keys = pygame.key.get_pressed()
         # keys[pygame.K_SPACE]:
